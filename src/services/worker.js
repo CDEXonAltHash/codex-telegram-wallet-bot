ccc1e3e082c2142af4c052130c2cfd3d29cac95a
Oanh Nguyen <friendforever0710@gmail.com>
2020-03-01 11:41:46 +0700

    crazy

--- src/services/worker.js
+++ src/services/worker.js
@@ -19,9 +19,8 @@ const handleJobQueue =  async ( data, done ) => {
             let balance = await getBalance(`${data.from}`);
             let unconfirmedBalance = balance.unconfirmedBalance;
             htmlbalanceunconfrim = unconfirmedBalance.toString().split('.');
-            while((htmlbalanceunconfrim[0])*1 <= -32 ) {
-               await sleep(120000)
-               break
+            if((htmlbalanceunconfrim[0])*1 <= -32 ) {
+               await sleep(300000)
             }
             await sendToken(`${data.from}`, data.volume, `${data.to}`, `${data.symbol}`)
         }
