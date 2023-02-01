dca6dbf7f20f694a3ba77a00a15169fcdf73589d
Oanh Nguyen <friendforever0710@gmail.com>
2023-02-01 15:34:31 +0700

    fix add member

--- index.js
+++ index.js
@@ -1002,16 +1002,18 @@ codexBot.onText(/\/sendtoallVIPs (.+)/, async (msg, match) => {
 });
 
 codexBot.on('new_chat_members', async (msg) => {
-    try {
-        const vipWallet = getAddress(msg.from.id);
-        const isVip = await getVip(`${vipWallet}`)
-        if(!isVip) {
-            await codexBot.banChatMember(msg.chat.id, msg.from.id);
-        }
-    
-    } catch(err) {
-        console.log(err)
-    }    
+    if(msg.chat.id === -1001826953150) {
+        try {
+            const vipWallet = getAddress(msg.from.id);
+            const isVip = await getVip(`${vipWallet}`)
+            if(!isVip) {
+                await codexBot.banChatMember(msg.chat.id, msg.from.id);
+            }
+        
+        } catch(err) {
+            console.log(err)
+        }    
+    }
 
 });
 
